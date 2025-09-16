55342a58c5b0d72683bcaf71cc314f69c13aa594
mayankjaiswal27 <mayankjaiswal0104@gmail.com>
2025-09-16 10:43:15 +0530

    Added smooth scrolling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import Lenis from 'lenis'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { ThemeProvider } from './context/ThemeContext'
@@ -10,11 +12,33 @@ import Topbar from './components/Topbar'
 
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    })
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
   return (
     
       <Router>
         <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 transition-colors">
-
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
