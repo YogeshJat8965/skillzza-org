054ff5d4128e6a7cf12fc882f1353be33c7b33c8
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-09-16 16:39:30 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ThemeProvider } from './context/ThemeContext'
-import ContactBar from './components/Contactbar'
+// import { ThemeProvider } from './context/ThemeContext'
+// import ContactBar from './components/Contactbar'
 import Topbar from './components/Topbar'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -11,7 +11,7 @@ import Footer from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <ContactBar />
+      {/* <ContactBar /> */}
       <Topbar />
       <Navbar />
       <App />
