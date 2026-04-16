55a0fa0fe04b955a84be57634948e4077a8a1040
yogesh <yogeshjat8965@gmail.com>
2026-04-16 14:32:59 +0530

    chore: configuration for vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/newsite/' : '/',
+  base: '/',
   server: {
     host: '127.0.0.1',
     port: 3000,
